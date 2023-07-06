8384bcf032ea4ffc9594e5c3acff7bd57f0289ea
hirozou <hiro3.hirozou@gmail.com>
2023-07-06 15:25:25 +0900

    update

--- network.py
+++ network.py
@@ -6,10 +6,18 @@ class Hopfield_Network:
         self.W = np.zeros((N, N))
 
     def fit(self, X):
-        self.W = map(lambda x: x @ x.T, X)
+        self.W = np.array(map(lambda x: x @ x.T, X))
 
     def energy_function(self, x):
         return -0.5 * np.dot(self.W, x @ x.T)
 
-    def predict(self, x):
+    def predict(self, x, iteration=100):
+        energies = []
+        x = x.reshape((-1, 1))
+        print(self.W.shape)
         y = self.W @ x
+        print(y.shape)
+        for _ in iteration:
+            y = self.W @ y.T
+            energies.append(self.energy_function(y))
+        return y
