2f2b0afb8e9aa6fdf053f36f10c358d9ab6b5a99
hirozou <hiro3.hirozou@gmail.com>
2023-07-18 15:40:43 +0900

    add gif

--- network.py
+++ network.py
@@ -35,6 +35,28 @@ class Hopfield_Network:
         
         return y
     
+    def recall(self, x, iter=10):
+        y_list = []
+
+        y = x.copy().reshape(-1)
+        self.energies.append(self.energy_function(y))
+        y_list.append(y.copy())
+
+        for i in range(iter):
+            print("iteration: {}".format(i))
+            print("")
+
+            for j in range(len(y)):
+                y[j] = np.sum(self.W[j] * y)
+                if (y[j] > 0):
+                    y[j] = 1
+                else:
+                    y[j] = -1
+                self.energies.append(self.energy_function(y))
+                y_list.append(y.copy())
+        
+        return y_list
+
     def plot_energies(self):
         fig = plt.figure()
         plt.plot(self.energies)
